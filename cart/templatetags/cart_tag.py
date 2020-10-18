95582766077d6cc5280de0893272536db61c8117
KieranCNZTech <kieran@cnztech.com>
2020-10-19 12:31:58 +1300

    Added useful/commonly used tags.

--- cart/templatetags/cart_tag.py
+++ cart/templatetags/cart_tag.py
@@ -1,8 +1,59 @@
 from django import template
+from typing import Union, List
+
+Number = Union[int, float]
 
 register = template.Library()
 
 
 @register.filter()
-def multiply(value, arg):
-    return float(value) * arg
+def subtotal(quantity: int, price: Number) -> float:
+    """
+    Returns a subtotal of the given inputs, quantity * price.
+    :param quantity:
+    :param price:
+    :return:
+    """
+    return int(quantity) * float(price)
+
+
+@register.filter
+def as_currency(amount: Number, currency_symbol: str = "$") -> str:
+    """
+    Formats the given amount as a currency.
+    :param amount: Number (int or float).
+    :param currency_symbol: The symbol for the given currency. Defaults to '$'.
+    :return: String in the format of "$200.00".
+    """
+    return "{}{:,.2f}".format(currency_symbol, float(amount))
+
+
+@register.filter
+def sum_list(a) -> float:
+    """
+    Gets the subtotal of each item in the cart.
+    :param a: List of products. Untyped as I'm unsure how 'dict_items' type truly works.
+    :return: Float.
+    """
+    total = 0
+    # a is of type 'dict_items'
+    for i, this_tuple in enumerate(a):
+        # key is product id, value is dictionary of product data
+        values = this_tuple[1]
+        qty = values['quantity']
+        price = values['price']
+        total += subtotal(qty, price)
+    return total
+
+
+@register.filter
+def cart_total(a, currency_symbol: str = "$") -> str:
+    """
+    Calculates the total of the given list of products and returns a
+        pretty representation.
+    :param a: List of products. Untyped as I'm unsure how 'dict_items' type truly works.
+    :param currency_symbol: The symbol for the given currency. Defaults to '$'.
+    :return: Str in the format of "$200.00".
+    """
+    total = sum_list(a)
+    return as_currency(total, currency_symbol)
