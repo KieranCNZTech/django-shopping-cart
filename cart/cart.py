4acde26a117b079abb5d9e9be7e12219e59c7ddb
KieranCNZTech <kieran@cnztech.com>
2020-10-19 09:52:52 +1300

    Cleaned up class and separated add and edit methods.

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,4 @@
-from decimal import Decimal
 from django.conf import settings
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
 
 
 class Cart(object):
@@ -9,49 +6,30 @@ class Cart(object):
     def __init__(self, request):
         self.request = request
         self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
-        if not cart:
-            # save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart
+        # Makes an empty cart if one does not exist then save it.
+        self.cart = self.session.get(settings.CART_SESSION_ID, {})
+        if self.cart == {}:
+            self.save()
 
-    def add(self, product, quantity=1, action=None):
+    def add(self, product, quantity: int = 1):
         """
-        Add a product to the cart or update its quantity.
+        Add a product to the cart.
+          If the product is already in the cart, will call edit_quantity.
+        :param product: Product to add to the cart. Stores the ID, name, and price.
+        :param quantity: How much of the product to add to the cart.
         """
-        id = product.id
-        newItem = True
+        p_id = product.id
         if str(product.id) not in self.cart.keys():
-
             self.cart[product.id] = {
                 'userid': self.request.user.id,
-                'product_id': id,
+                'product_id': p_id,
                 'name': product.name,
-                'quantity': 1,
-                'price': str(product.price),
-                
+                'quantity': quantity,
+                'price': str(product.price)
             }
+            self.save()
         else:
-            newItem = True
-
-            for key, value in self.cart.items():
-                if key == str(product.id):
-
-                    value['quantity'] = value['quantity'] + 1
-                    newItem = False
-                    self.save()
-                    break
-            if newItem == True:
-
-                self.cart[product.id] = {
-                    'userid': self.request,
-                    'product_id': product.id,
-                    'name': product.name,
-                    'quantity': 1,
-                    'price': str(product.price),
-                }
-
-        self.save()
+            self.edit_quantity(product, quantity)
 
     def save(self):
         # update the session cart
@@ -63,22 +41,25 @@ class Cart(object):
         """
         Remove a product from the cart.
         """
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
+        p_id = str(product.id)
+        if p_id in self.cart:
+            del self.cart[p_id]
             self.save()
 
-    def decrement(self, product):
-        for key, value in self.cart.items():
-            if key == str(product.id):
-
-                value['quantity'] = value['quantity'] - 1
-                if(value['quantity'] < 1):
-                    return redirect('cart:cart_detail')
-                self.save()
-                break
+    def edit_quantity(self, product, new_qty: int):
+        """
+        Edits the amount of a product in the cart.
+          If the product is not in the cart, will call add.
+        :param product: Product to edit the quantity of.
+        :param new_qty: New quantity to give to the product. Will remove the product is this reaches 0.
+        """
+        p_id = product.id
+        if str(p_id) in self.cart.keys():
+            if new_qty == 0:
+                self.remove(product)
             else:
-                print("Something Wrong")
+                self.cart[product.id]['quantity'] = new_qty
+                self.save()
 
     def clear(self):
         # empty cart
