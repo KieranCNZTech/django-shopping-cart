ba5f053eeee2814da24fecfef6fd7bf84bb68f66
KieranCNZTech <kieran@cnztech.com>
2020-10-20 10:07:26 +1300

    as_currency back to filter

--- cart/templatetags/cart_tag.py
+++ cart/templatetags/cart_tag.py
@@ -17,7 +17,7 @@ def subtotal(quantity: int, price: Number) -> float:
     return int(quantity) * float(price)
 
 
-@register.simple_tag
+@register.filter
 def as_currency(amount: Number, rate: float = 1) -> str:
     """
     Formats the given amount as a currency.
